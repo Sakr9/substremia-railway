4394e0ab348e0657ea75958b1baeac9a157b61d8
Francesco <fradan95@gmail.com>
2025-03-29 23:56:35 +0100

    Fix: Update healthcheck and remove Firebase from server.js

--- server/server.js
+++ server/server.js
@@ -2,15 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { admin, db } = require('./utils/firebase');
 const path = require('path');
 const { getRouter } = require('stremio-addon-sdk');
 const { subtitlesHandler } = require('../addon/subtitles.handler');
 const addonInterface = require('../addon/index');
 const multer = require('multer');
+const db = require('./utils/db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Inizializza il database
+db.initializeDatabase().catch(console.error);
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -32,7 +35,9 @@ app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
-const server = app.listen(PORT);
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 process.on('SIGINT', () => {
   server.close(() => {
